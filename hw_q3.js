197c91b762ba8a02ee8c23a7b15da68d1262b37c
raj89er <134079776+raj89er@users.noreply.github.com>
2024-04-09 16:16:00 -0400

    background theme button works

--- hw_q3.js
+++ hw_q3.js
@@ -7,11 +7,31 @@ console.log('#'.repeat(77));
 // Add a click event listener to your button that will change the background color of the body
 // The background should toggle between at least 2 colors
 
-const body = document.querySelector('body');
-const button = document.createElement('button');
 
-button.id = 'backgroundChanger';
-button.textContent = 'Change Background';
+const pokemonData = [{
+    color: '#8BC33E',
+    name: 'Chikorita',
+    hail: 'Chikorita is a loyal and compassionate partner, known for its healing abilities.'
+},{
+    color: '#FF9C52',
+    name: 'Cyndaquil',
+    hail: 'Cyndaquil is a passionate fighter, capable of unleashing powerful flames against foes.'
+},{
+    color: '#6EC0E4',
+    name: 'Totodile',
+    hail: 'Totodile is a playful and energetic Pokémon, famous for its powerful jaw strength.'
+}];
 
-body.appendChild(button);
+const button = document.getElementById('backgroundChanger');
+let currentIndex = 0;
+const hailDisplay = document.getElementById('hailDisplay');
+
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = pokemonData[currentIndex].color;
+    hailDisplay.innerHTML = pokemonData[currentIndex].hail;
+
+    currentIndex = (currentIndex + 1) % pokemonData.length;
+});
+
+
 
